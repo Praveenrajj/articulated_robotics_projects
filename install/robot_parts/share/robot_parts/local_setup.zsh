afbec696fbbc0d761fd6e1f382c67e5e05f2d6fc
Praveenrajj <praveenrajramesh@vt.edu>
2025-08-16 02:18:26 -0400

    this is for the gazebo

---
+++ install/robot_parts/share/robot_parts/local_setup.zsh
@@ -0,0 +1,1 @@
+/home/praveen/two_wheeled_robot/src/build/robot_parts/ament_cmake_environment_hooks/local_setup.zsh
--- robot_parts/launch/twr_spawn.launch.py
+++ robot_parts/launch/twr_spawn.launch.py
@@ -2,6 +2,8 @@ import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 import xacro
 
 def generate_launch_description():
@@ -17,12 +19,36 @@ def generate_launch_description():
         parameters=[{'robot_description':robot_description,'use_sim_time':True}]
     )
 
+ 
+
     jsp= Node(
         package="joint_state_publisher_gui",
         executable="joint_state_publisher_gui",
         name="joint_state_publisher_gui",
         output="screen"
     )
+
+
+    gazebo = IncludeLaunchDescription(
+    PythonLaunchDescriptionSource(
+        os.path.join(get_package_share_directory('gazebo_ros'), 'launch', 'gazebo.launch.py')
+    ),
+    launch_arguments={
+        'world': os.path.join(get_package_share_directory(pkg_name), 'worlds', 'cardboard_obstacle.world')
+    }.items()
+)
+    
+
+    spawn_entity= Node(
+        package="gazebo_ros",
+        executable="spawn_entity.py",
+        arguments=[
+            "-entity", "two_wheeled_robot",
+            "-topic", "robot_description"
+        ],
+        output="screen"
+    )
+
 
     # Launch RViz2
     rviz_node = Node(
@@ -35,6 +61,8 @@ def generate_launch_description():
 
     return LaunchDescription([
         node_rsp,
+        gazebo,
+        spawn_entity,
         rviz_node,
-        jsp
+        # jsp
     ])
